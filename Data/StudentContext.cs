709c508fcc910b6d01c98317b6f55b1be7a384ce
Hùng Phạm <hunghyperx@gmail.com>
2025-06-09 05:13:40 +0700

    4th commit

--- Data/StudentContext.cs
+++ Data/StudentContext.cs
@@ -8,5 +8,24 @@ namespace ApiSinhVien.Data
         public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
 
         public DbSet<Student> Students { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Student>().ToTable("students"); // dùng chữ thường để khớp PostgreSQL
+
+        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("students"); // Tên bảng viết thường
+
+                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Uid).HasColumnName("uid");
+                entity.Property(e => e.Name).HasColumnName("name");
+                entity.Property(e => e.Class).HasColumnName("class");
+                entity.Property(e => e.Department).HasColumnName("department");
+            });
+        }
     }
 }
--- obj/Debug/net8.0/ApiSinhVien.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiSinhVien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiSinhVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a890d12bb12429d502035b97ad9cb3424360a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00663e749f16f24571aea401293ead33f74bc3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiSinhVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiSinhVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
